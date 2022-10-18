448936f09e87cc8683b8fda108cf703233c8743c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-10-18 06:31:02 +0000

    Deploying to gh-pages from @ LTLA/uzuki2@361d3a8b7f1063b4bd0f827f3dbb8f3ace3ba351 🚀

--- docs/search/all_c.js
+++ docs/search/all_c.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['validate_0',['validate',['../namespaceuzuki2.html#aee7fbf7746e1064f4393cbeea912cc15',1,'uzuki2::validate(const H5::Group &amp;handle, const std::string &amp;name, int num_external=0)'],['../namespaceuzuki2.html#a262203b54a9a3a054b0df9cbd8ad6105',1,'uzuki2::validate(const std::string &amp;file, const std::string &amp;name, int num_external=0)']]],
   ['validate_2ehpp_1',['validate.hpp',['../validate_8hpp.html',1,'']]],
-  ['vector_2',['Vector',['../structuzuki2_1_1Vector.html',1,'uzuki2']]]
+  ['validate_5fjson_2',['validate_json',['../namespaceuzuki2.html#a2fe5c875d74ceb2a2bda095a4ef5e771',1,'uzuki2::validate_json(const std::string &amp;file, int num_external=0)'],['../namespaceuzuki2.html#a6693da2cc6e89cd49711a915b23751c0',1,'uzuki2::validate_json(const unsigned char *buffer, size_t len, int num_external=0)']]],
+  ['vector_3',['Vector',['../structuzuki2_1_1Vector.html',1,'uzuki2']]]
 ];
