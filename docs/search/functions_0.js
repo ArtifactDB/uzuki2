a8eba7fdf6dff4c3182736624652819a9f8cc62a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-02-20 05:35:39 +0000

    Deploying to gh-pages from @ LTLA/uzuki2@bf0a8da2168d46dd27024b05a4574925f038d6ae 🚀

--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['is_5fordered_0',['is_ordered',['../structuzuki2_1_1Factor.html#a18216ec442a42e03a167af2f7802ee0d',1,'uzuki2::Factor']]],
-  ['is_5fvector_1',['is_vector',['../namespaceuzuki2.html#a66472c68f697e7f6e1ea576a56cb2de8',1,'uzuki2']]]
+  ['is_5fscalar_1',['is_scalar',['../structuzuki2_1_1TypedVector.html#aa84e38c7276083a50d5abc39b3ca18fb',1,'uzuki2::TypedVector']]],
+  ['is_5fvector_2',['is_vector',['../namespaceuzuki2.html#a66472c68f697e7f6e1ea576a56cb2de8',1,'uzuki2']]]
 ];
