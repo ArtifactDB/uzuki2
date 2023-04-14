f2b76d8f3cd8af2dada0c510a3b4f3c7db63b606
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-04-14 22:51:37 +0000

    Deploying to gh-pages from @ LTLA/uzuki2@fceb37ef0ecf48bc6d29e9c6c4cb5c6d18636fb8 🚀

--- docs/search/typedefs_1.js
+++ docs/search/typedefs_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['datevector_0',['DateVector',['../namespaceuzuki2.html#ad1cc497d1c24bacb9b560426a57a4979',1,'uzuki2']]]
+  ['datetimevector_0',['DateTimeVector',['../namespaceuzuki2.html#a84fd2adf03a7d3b07214d3343dde8bad',1,'uzuki2']]],
+  ['datevector_1',['DateVector',['../namespaceuzuki2.html#ad1cc497d1c24bacb9b560426a57a4979',1,'uzuki2']]]
 ];
