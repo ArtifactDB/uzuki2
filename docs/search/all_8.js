09673e0de3215bede433027589918f76d7b73116
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-02-05 03:40:31 +0000

    Deploying to gh-pages from @ ArtifactDB/uzuki2@59c48465e571b77cc312d6c9cbdc6590283d0492 🚀

--- docs/search/all_8.js
+++ docs/search/all_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['options_0',['options',['../structuzuki2_1_1hdf5_1_1Options.html',1,'uzuki2::hdf5::Options'],['../structuzuki2_1_1json_1_1Options.html',1,'uzuki2::json::Options']]],
+  ['options_0',['Options',['../structuzuki2_1_1hdf5_1_1Options.html',1,'uzuki2::hdf5::Options'],['../structuzuki2_1_1json_1_1Options.html',1,'uzuki2::json::Options']]],
   ['or_20json_1',['R lists to HDF5 or JSON',['../index.html',1,'']]]
 ];
