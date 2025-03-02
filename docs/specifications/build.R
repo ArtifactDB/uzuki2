c3c38cee5cccff64e5f26e365d3495a9c2db2b1c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-03-01 20:24:02 -0800

    Fixes to the docs.

--- docs/specifications/build.R
+++ docs/specifications/build.R
@@ -1,7 +1,7 @@
 library(knitr)
 dir.create("compiled", showWarnings=FALSE)
 
-for (v in c("1.0", "1.1", "1.2", "1.3")) {
+for (v in c("1.0", "1.1", "1.2", "1.3", "1.4")) {
     .version <- package_version(v)
     knitr::knit("hdf5.Rmd", output=file.path("compiled", paste0("hdf5-", v, ".md")))
 }
